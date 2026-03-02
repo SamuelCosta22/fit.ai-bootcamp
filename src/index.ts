cd7e5ef5771809308315f31257cd66521ef2a4b6
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2026-03-02 15:47:33 -0300

    feat: ✨ add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World");
+import "dotenv/config";
+
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", function (request, reply) {
+  reply.send({ hello: "world" });
+});
+
+fastify.listen({ port: Number(process.env.PORT) || 3333 }, function (err) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
