9c469fca3d465e60937c58342c64aceea4a730bc
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2026-03-02 15:53:53 -0300

    chore: 🔧 add zod and fastify-type-provider-zod setup

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,42 @@
 import "dotenv/config";
 
 import Fastify from "fastify";
-const fastify = Fastify({
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import z from "zod";
+
+const app = Fastify({
   logger: true,
 });
 
-fastify.get("/", function (request, reply) {
-  reply.send({ hello: "world" });
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/",
+  schema: {
+    description: "Hello World",
+    tags: ["hello"],
+    response: {
+      200: z.object({
+        message: z.string(),
+      }),
+    },
+  },
+  handler: () => {
+    return {
+      message: "Hello World",
+    };
+  },
 });
 
-fastify.listen({ port: Number(process.env.PORT) || 3333 }, function (err) {
+app.listen({ port: Number(process.env.PORT) || 3333 }, function (err) {
   if (err) {
-    fastify.log.error(err);
+    app.log.error(err);
     process.exit(1);
   }
 });
